68c66d334a19d739b8f4c8722c16934f6e6c525d
Quentin Geerts <quentin.geerts@bstorm.be>
2026-08-18 23:02:59 +0200

    Initial commit

---
+++ src/models/Statistiques.java
@@ -0,0 +1,110 @@
+package models;
+
+import enums.Genre;
+import services.Mediatheque;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Statistique {
+    private final String nomMediatheque;
+    private final int totalMedias;
+    private final int nbLivres;
+    private final int nbDvd;
+    private final int nbJeux;
+    private final int nbDisponibles;
+    private final int nbEmpruntes;
+    private final int nbMembres;
+    private final Genre genreDominant;
+    private final int nbGenreDomimant;
+
+    public Statistique(Mediatheque mediatheque) {
+        nomMediatheque = mediatheque.getNom();
+        totalMedias = mediatheque.listerTous().size();
+        nbMembres = mediatheque.listerTous().size();
+
+        int livres = 0;
+        int dvd = 0;
+        int jeux = 0;
+        int disponibles = 0;
+        Map<Genre, Integer> parGenre = new HashMap<>();
+
+        for (Media media : mediatheque.listerTous()) {
+            if (media instanceof Livre) livres++;
+            else if (media instanceof Dvd) dvd++;
+            else if (media instanceof JeuVideo) jeux++;
+
+            if (media.isDisponible()) disponibles++;
+
+            // Combien par genre ? 0 si aucun puis on ajoute 1
+            Genre g = media.getGenre();
+            parGenre.put(g, parGenre.getOrDefault(g, 0) + 1);
+        }
+
+        nbLivres = livres;
+        nbDvd = dvd;
+        nbJeux = jeux;
+        nbDisponibles = mediatheque.listerDisponible().size();
+        nbEmpruntes = totalMedias - nbDisponibles;
+
+
+        // Dominant
+        Genre dominant = null;
+        int meilleur = 0;
+
+        for (Genre g : parGenre.keySet()) {
+            int combien = parGenre.get(g);
+            if (combien > meilleur) {
+                dominant = g;
+                meilleur = combien;
+            }
+        }
+
+        genreDominant = dominant;
+        nbGenreDomimant = meilleur;
+
+    }
+
+    // Getters
+
+
+    public String getNomMediatheque() {
+        return nomMediatheque;
+    }
+
+    public int getTotalMedias() {
+        return totalMedias;
+    }
+
+    public int getNbLivres() {
+        return nbLivres;
+    }
+
+    public int getNbDvd() {
+        return nbDvd;
+    }
+
+    public int getNbJeux() {
+        return nbJeux;
+    }
+
+    public int getNbDisponibles() {
+        return nbDisponibles;
+    }
+
+    public int getNbEmpruntes() {
+        return nbEmpruntes;
+    }
+
+    public int getNbMembres() {
+        return nbMembres;
+    }
+
+    public Genre getGenreDominant() {
+        return genreDominant;
+    }
+
+    public int getNbGenreDomimant() {
+        return nbGenreDomimant;
+    }
+}
